3ee8d44b517e8403d1e965b3d7dae19fcf1b3584
Cory Sparks <sparks-40067@cotc.edu>
2017-01-16 13:42:52 -0500

    Broker

---
+++ Broker/Broker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace broker
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Process process = new Process();
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            string userName = Environment.UserName;
+            Debug.WriteLine(userName);
+            info.FileName = "C:/Users/Public/Downloads/Python27/python.exe";
+            info.Arguments = "C:/Users/Public/Downloads/x.pyw";
+            process.StartInfo = info;
+            process.Start();
+            Console.ReadLine();
+        }
+    }
+}
